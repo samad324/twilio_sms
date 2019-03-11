184dc3098c8d1ab5f4b03d5db49da431d6653112
samad324 <a.s3248889@gmail.com>
2019-03-11 12:22:52 +0500

    Twilio sms service

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")();
 const bodyPaser = require("body-parser");
 const twilio = require("twilio");
-const accountSid = "AC37cd9764ba196b6621ea8f71a7e89ef6";
-const authToken = "b8dece854e6529b0e60d287ebb165aec";
+
+const accountSid = "TWILIO_SID";
+const authToken = "TWILIO_AUTH_TOKEN";
 
 express.use(bodyPaser());
 
@@ -11,13 +12,12 @@ express.get("/signup", (req, res) => {
 });
 
 express.post("/sms", (req, res) => {
-  console.log(req.body);
   const client = twilio(accountSid, authToken);
 
   client.messages
     .create({
       body: req.body.subject,
-      from: "+18507357229",
+      from: "Your twilio phone number.",
       to: req.body.to
     })
     .then(message => res.send(message.sid))
